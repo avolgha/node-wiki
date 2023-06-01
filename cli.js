7df0b808e3045181243a842980b1bc1883d84c09
avolgha <weilandmarius@gmail.com>
2023-06-01 12:01:46 +0200

    inital commit

---
+++ cli.js
@@ -0,0 +1,160 @@
+import blessed from "blessed";
+import * as docs from "./docs.js";
+
+const screen = blessed.screen({
+	smartCSR: true,
+});
+
+screen.title = "node-wiki";
+
+const box = blessed.box({
+	top: "center",
+	left: "center",
+	width: "80%",
+	height: "80%",
+	content: "nothing",
+	border: { type: "line" }
+});
+
+let qListener;
+const q = (fn) => {
+	qListener = fn;
+	box.key("q", qListener);
+}
+function freeBox() {
+	box.children.forEach((child) => {
+		box.remove(child);
+	});
+
+	if (qListener) {
+		box.unkey("q", qListener);
+	}
+}
+
+function renderError(error) {
+	freeBox();
+
+	throw new Error("method not implemented");
+}
+
+function renderVersionSelect() {
+	freeBox();
+
+	const infoText = blessed.text({
+		content: "{bold}select a node.js version{/}",
+		tags: true,
+	});
+
+	const select = blessed.list({
+		top: "5%",
+		mouse: true,	
+		keys: true,
+		vi: true,
+		style: {
+			selected: {
+				bg: "red",
+			}
+		}
+	});
+
+	const items = [
+		"18.x",
+		"20.x",
+	];
+	select.setItems(items);
+
+	select.on("select", (_, idx) => {
+		const version = items[idx];
+	
+		if (docs.isDocInstalled(version)) {
+			renderChooseFile(version);
+		} else {
+			renderDownloadVersion(version, renderChooseFile);
+		}
+	});
+
+	box.append(infoText);
+	box.append(select);
+
+	q(() => process.exit(0));
+
+	select.focus();
+}
+
+function renderDownloadVersion(version, thenFn) {
+	freeBox();
+	
+	const infoText = blessed.bigText({
+		top: "center",
+		left: "center",
+		content: "{bold}downloading version...{/}",
+	});
+
+	box.append(infoText);
+
+	docs.downloadDocs(version, true).then((error) => {
+		if (error) {
+			renderError(error);
+			return;
+		}
+
+		thenFn(version);
+	});
+}
+
+function renderChooseFile(version) {
+	freeBox();
+
+	const infoText = blessed.text({
+		content: "{bold}select a file in the documentation{/}",
+		tags: true,
+	});
+
+	const select = blessed.list({
+		top: "5%",
+		mouse: true,	
+		keys: true,
+		vi: true,
+		style: {
+			selected: {
+				bg: "red",
+			}
+		}
+	});
+
+	const items = docs.listFiles(version);
+	select.setItems(items);
+
+	select.on("select", (_, idx) => {
+		const file = items[idx];
+
+		renderFileContent(version, file);
+	});
+
+	box.append(infoText);
+	box.append(select);
+
+	q(() => renderVersionSelect());
+	
+	select.focus();
+}
+
+export function renderFileContent(version, file) {
+	freeBox();
+
+	const content = docs.readDoc(version, file);
+	const text = blessed.text({ content });
+
+	box.append(text);
+
+	q(() => renderChooseFile(version));
+
+	text.focus();
+}
+
+renderVersionSelect();
+screen.append(box);
+
+screen.key(["escape"], () => process.exit(0));
+
+screen.render();
---
+++ docs.js
@@ -0,0 +1,74 @@
+import asar from "@electron/asar";
+import fs from "fs";
+import os from "os";
+import path from "path";
+import { request } from "undici";
+
+export function getDocUrl(version) {
+	return `https://raw.githubusercontent.com/avolgha/node-wiki/docs/${version}.asar`;
+}
+
+export const docSavePath = path.join(os.homedir(), ".config", "node-wiki");
+export function getDocPath(version) {
+	return path.join(docSavePath, `${version}.asar`);
+}
+
+export function isDocInstalled(version) {
+	return fs.existsSync(getDocPath(version));
+}
+
+// a returned string represents an error.
+export async function downloadDocs(version, force = false) {
+	const fpath = getDocPath(version);
+
+	if (isDocInstalled(version)) {
+		if (!force) return;
+
+		fs.rmSync(fpath);
+	}
+
+	if (!fs.existsSync(docSavePath)) {
+		fs.mkdirSync(docSavePath);
+	}
+
+	const url = getDocUrl(version);
+	const response = await request(url);
+
+	if (response.statusCode !== 200) {
+		return `request ended with status code ${response.statusCode} ("${url}")`;
+	}
+
+	const body = await response.body.text();
+	fs.writeFileSync(path, body);
+
+	return null;
+}
+
+export function listDownloadedDocs() {
+	if (!fs.existsSync(docSavePath)) {
+		return [];
+	}
+
+	return fs.readdirSync(docSavePath)
+		.filter((file) => fs.statSync(file).isFile() && file.endsWith(".asar"))
+		.map((file) => file.replace(/\.asar$/g, ""));
+}
+
+export function listFiles(version) {
+	if (!isDocInstalled(version)) {
+		return null;
+	}
+
+	return asar.listPackage(getDocPath(version));
+}
+
+export function readDoc(version, file) {
+	if (!isDocInstalled(version)) {
+		return null;
+	} else if (!listFiles(version).includes(file)) {
+		return null;
+	}
+
+	const content = asar.extractFile(getDocPath(version), file);
+	return content.toString();
+}
